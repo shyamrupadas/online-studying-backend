59b3aa2c82ad4b78d8c5c6529af8caee396affa5
Sergey Maksimov <s.mksimov@gmail.com>
2022-01-17 23:53:36 +0300

    add: user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.k5gh1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')]
 })
 export class AppModule {}

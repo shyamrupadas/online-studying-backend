dc68435f451b9beb7da1078b965eeb4cd5146d46
Sergey Maksimov <s.mksimov@gmail.com>
2022-01-17 23:14:55 +0300

    refactor: main

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const start = async () => {
-  const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
 
-  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 start();
